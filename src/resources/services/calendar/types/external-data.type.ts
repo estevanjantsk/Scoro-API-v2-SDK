507b99cc0ff01da33f72338384b939a33ad1b055
Estevan Jantsk <estevan.jantsk@scoro.com>
2025-04-08 10:19:43 +0300

    chore: update version to 1.10.0 and refine external_meta_data type

--- src/resources/services/calendar/types/external-data.type.ts
+++ src/resources/services/calendar/types/external-data.type.ts
@@ -7,5 +7,5 @@ export interface IExternalData {
   external_guests?: string[] // Array of external guests linked to calendar event
   external_repeat_id?: string // ID of the parent event in case of a repeated event
   external_repeat_uid?: string // UID of the parent event in case of a repeated event
-  external_meta_data?: unknown[] // External meta data, not filterable
+  external_meta_data?: Record<string, unknown> // External meta data object with unknown keys and values, not filterable
 }
